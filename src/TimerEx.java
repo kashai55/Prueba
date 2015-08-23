d4ed79fde3e0e65fcb09b53a6bb9fe9c6d402c41
kashai55 <fankashai@hotmail.com>
2015-08-23 00:28:23 -0600

    Es un timer segundo a segundo hasta llegar a 10 (primer commit que hago)

---
+++ src/TimerEx.java
@@ -0,0 +1,20 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+class TimerEx {
+    public static void iniciar() {
+    Timer timer;
+    timer = new Timer();
+
+    TimerTask task = new TimerTask() { //aqui se declara la tarea que se va a realizar cada cierto tiempo
+    	int i=0;
+        public void run(){
+        	System.out.println(i);
+        	i++;
+        	if (i==11){System.out.print("termino");timer.cancel();}
+        }
+    };
+    
+    timer.schedule(task,0,1000); //task es la tarea a realizar, 0 son los milisegundos que espera antes de realizar la tarea y 1000 es la cantidad de milisegundos que espera antes de volver a hacer la tarea especificada
+    }
+}
